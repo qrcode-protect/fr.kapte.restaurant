91973346885bc222761a37501dab67dbfe543b46
alpha SOW <alphaseul@yahoo.fr>
2022-02-16 15:12:51 +0100

    first commit

--- public_flutter/flutter_service_worker.js
+++ public_flutter/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "d89e56d7446d75c2c4c129c5f5222859",
-"index.html": "f829a01bfe8087da9a0f726965d38528",
-"/": "f829a01bfe8087da9a0f726965d38528",
-"main.dart.js": "7380edd9b0d1ae9d2f98a7e6b1fe8d5e",
+"index.html": "f0109f2b02c96e402da7bd27ae4af7e0",
+"/": "f0109f2b02c96e402da7bd27ae4af7e0",
+"main.dart.js": "39d479f9ff011f7844ab79f7732459ae",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
